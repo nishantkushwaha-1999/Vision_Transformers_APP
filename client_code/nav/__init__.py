5d5a87e713dddedbebfb50c9a3907c984fb920b7
nskp.sec19@outlook.com <nskp.sec19@outlook.com>
2024-01-28 18:04:39 +0000

    Edited nav.basicCNN and nav

--- client_code/nav/__init__.py
+++ client_code/nav/__init__.py
@@ -5,17 +5,9 @@ from basicCNN import basicCNN
 
 class nav(navTemplate):
   def __init__(self, **properties):
-
-    # Set Form properties and Data Bindings.
-
     self.init_components(**properties)
 
-
-
-    # Any code you write here will run before the form opens.
-
   def BasicCNN_click(self, **event_args):
-    """This method is called when the button is clicked"""
     self.main.clear()
     self.main.add_component(basicCNN())
     
--- client_code/nav/basicCNN/__init__.py
+++ client_code/nav/basicCNN/__init__.py
@@ -1,9 +1,9 @@
 from ._anvil_designer import basicCNNTemplate
 from anvil import *
+import plotly.graph_objects as go
 
 class basicCNN(basicCNNTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
