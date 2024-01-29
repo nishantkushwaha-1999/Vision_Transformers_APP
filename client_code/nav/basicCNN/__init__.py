8b45cb0944985f77987db09faa09ffbcf53fe161
nskp.sec19@outlook.com <nskp.sec19@outlook.com>
2024-01-29 18:02:45 +0000

    Edited nav.basicCNN

--- client_code/nav/basicCNN/__init__.py
+++ client_code/nav/basicCNN/__init__.py
@@ -7,3 +7,7 @@ class basicCNN(basicCNNTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
