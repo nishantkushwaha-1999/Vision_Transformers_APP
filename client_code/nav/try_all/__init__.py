51bb7e811362fcdde6791a134dc96e55851bbf9f
nskp.sec19@outlook.com <nskp.sec19@outlook.com>
2024-02-12 20:49:32 +0000

    Edited nav.try_all

--- client_code/nav/try_all/__init__.py
+++ client_code/nav/try_all/__init__.py
@@ -13,8 +13,19 @@ class try_all(try_allTemplate):
   def file_loader_1_change(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
     # try:
+    print(anvil.server.call("get_history", 'basicCNN', 'val'))
+    print(anvil.server.call("get_history", 'basicCNN', 'all'))
+    print(anvil.server.call("get_history", 'ViT', 'val'))
+    print(anvil.server.call("get_history", 'ViT', 'all'))
+
+    print(anvil.server.call("evaluate", 'basicCNN'))
+    print(anvil.server.call("evaluate", 'vit'))
+
     im = anvil.server.call("image", file)
     self.image_1.source = im
+
+    print(anvil.server.call("predict", 'basicCNN'))
+    print(anvil.server.call("predict", 'vit'))
     # result = anvil.server.call("predict_vit", file)
     # print('ViT', result)
     # except ValueError:
